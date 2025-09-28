9d52f1ed539f267039f43f8a91bf06c1360841b6
Satvik1704 <rsatvik8800@gmail.com>
2025-09-29 00:41:02 +0530

    added logout feature and the contoller middleware for cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -147,10 +147,39 @@ return res
 
 
 
+const logoutUser = asyncHandler(async(req,res) =>{
+await User.findByIdAndUpdate(
+req.user._id,{
+    $set:{
+        refreshToken: ""
+    },
+},
+{
+
+    new: true,
+
+
+},
+
+);
+
+const options = {
+    httpOnly: true,
+    secure: true
+}
+return res
+  .status(200)
+  .clearCookie("access token",options)
+  .clearCookie("refreshToken", options)
+  .json(
+    new ApiResponse(200, {}, "user logged out")
+  )
+
+
+});
 
 
 
-
-export { registerUser, login };
+export { registerUser, login, logoutUser };
 
 
